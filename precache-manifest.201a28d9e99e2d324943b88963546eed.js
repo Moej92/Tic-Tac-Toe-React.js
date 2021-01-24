d895be8b0775f8175b6f517ce551e658f1513662
Mohammed Janeb <mohammedjaneb92@gmail.com>
2021-01-24 17:09:43 +0200

    Updates

---
+++ precache-manifest.201a28d9e99e2d324943b88963546eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fba33de08e972af72ef1d06341eaf8f",
+    "url": "/Tic-Tac-Toe-React.js/index.html"
+  },
+  {
+    "revision": "f0c32815a496b14e8f45",
+    "url": "/Tic-Tac-Toe-React.js/static/css/main.141ed84b.chunk.css"
+  },
+  {
+    "revision": "6cd97ae52b00a32b8ed1",
+    "url": "/Tic-Tac-Toe-React.js/static/js/2.5aabfbdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React.js/static/js/2.5aabfbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c32815a496b14e8f45",
+    "url": "/Tic-Tac-Toe-React.js/static/js/main.1e57e623.chunk.js"
+  },
+  {
+    "revision": "f295e6296b12cff802b3",
+    "url": "/Tic-Tac-Toe-React.js/static/js/runtime-main.17627b5b.js"
+  }
+]);
